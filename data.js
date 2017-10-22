9a194392734e57e0c426e45eb15e50f8beae442d
garevna <garevna@mail.ru>
2017-10-22 10:21:29 +0300

    for testing perpose only

---
+++ data.js
@@ -0,0 +1,65 @@
+var routes_data = [
+  {
+    name:"Irina",
+    title:"Ведьма",
+    comment:"Играет на скрипке, бьет баклуши, считает ворон",
+    ref:"",
+    picture:"https://cdn-images-1.medium.com/max/1200/1*yeAO-nwsAqnzr7k-zoDkoQ.png"
+  },
+  {
+    name:"Ivan",
+    title:"Тот еще фрукт",
+    comment:"Любит жареные пирожки и печеные яблоки",
+    ref:"",
+    picture:"http://www.istockphoto.com/resources/images/PhotoFTLP/img_82250973.jpg"
+  },
+  {
+    name:"Dmitro",
+    title:"Пижон",
+    comment:"Может шевелить ушами и не дышать под водой 2 минуты",
+    ref:"",
+    picture:"https://static.pexels.com/photos/235621/pexels-photo-235621.jpeg"
+  },
+]
+
+var usersPosts = [];
+
+usersPosts ["Irina"] = [
+  {
+    head: "Веселые каникулы",
+    pict: "http://www.istockphoto.com/resources/images/PhotoFTLP/img_67920257.jpg",
+    text: `Нужно почаще выбираться на природу\n
+           Каждый раз какие-нибудь приключения`
+  },
+  {
+    head: "Холодная зима",
+    pict: "http://brokeassstuart.com/ny/files/2011/01/New-York-Central-Park-.jpg",
+    text: `Я не люблю зиму\n
+           Холодно, безжизненно, темно...\n
+           Хочется в берлогу и проспать до весны`
+  }
+]
+usersPosts ['Ivan'] = [{
+  head: "Graphic photos",
+  pict: "https://onthe.rocks/wp-content/uploads/2015/09/great-view.jpg",
+  text: `Graphic photos released Friday allow the public to see for the first time 
+        how Chicago teen Kenneka Jenkins was found dead in a freezer, but her family 
+        says the images raise more questions than answers.\n
+        The photos, while disturbing, help provide more insight into the case, which 
+        has been wrapped in controversy, rumors and conspiracy theories since the 
+        19-year-old was found dead on September 10 in a walk-in freezer at the Crowne 
+        Plaza hotel in Rosemont, a village 17 miles northwest of Chicago.\n`
+}]
+usersPosts ['Dmitro'] = [{
+  head: "Dmitro says:",
+  pict: "https://iso.500px.com/wp-content/uploads/2016/04/stock-photo-142871511-1500x1000.jpg",
+  text: `Android O’s new navigation bar customizer, 
+        accessible through SystemUI Tuner, allows you to set a keycode 
+        to a navigation key. (Reminder: in order to access SystemUI Tuner, 
+        you have to pull down the status bar and long-press on the gear icon in the 
+        top right until you see a toast message telling you that SystemUI Tuner is 
+        now accessible.) Because there are so many keycodes, Android O does not 
+        offer an icon for each keycode you can place on the nav bar, but instead 
+        allows you to select from 6 icons: circle, plus, minus, left, right, and 
+        menu.`
+}]
