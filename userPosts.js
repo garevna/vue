c5234135319717ab3bb43f7c46a7b27575d3a117
garevna <garevna@mail.ru>
2017-10-22 22:46:32 +0300

    Update userPosts.js

--- userPosts.js
+++ userPosts.js
@@ -20,10 +20,27 @@ const UserPosts = {
     }
   },
   template: `<section class="article">
-                <div v-for="item in currentUserPosts">
-                  <h3>{{item.head}}</h3>
-                  <img :src="item.pict"/>
-                  <p>{{item.text}}</p>
+              <transition-group name="articles" tag="div">
+                <div v-for="item in currentUserPosts"
+                      :key="item.head">
+                  <transition name="articles" tag="div">
+                  <h3>{{ item.head }}</h3>
+                  </transition>
+                  <transition name="articles" tag="div">
+                  <img v-if="item.pict" :src="item.pict"/>
+                  </transition>
+                  <transition name="articles" tag="div">
+                  <pre>{{ item.code }}</pre>
+                  </transition>
+                  <p>{{ item.text }}</p>
+                  <a v-if="item.ref"
+                      v-for="sample in item.ref"
+                      class="menu-item"
+                      :href="sample" target="_blank">
+                    <span :v-if="item.ref">Пример</span>
+                  </a>
+                  <hr/>
                 </div>
+              </transition-group>
              </section>`
 }
