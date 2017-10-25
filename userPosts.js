bdee4b968c39a751baa3a324244684a5cfd59bd5
garevna <garevna@mail.ru>
2017-10-25 08:54:03 +0300

    Update userPosts.js

--- userPosts.js
+++ userPosts.js
@@ -1,7 +1,9 @@
 const UserPosts = {
   props:[ 'currentUser' ],
   data: function () {
-    return { currentUserPosts: this.$parent.articles }
+    return {
+      currentUserPosts: this.$parent.articles
+    }
   },
   mounted: function () {
     this.sendUserEvent ( 'posts' )
@@ -17,30 +19,63 @@ const UserPosts = {
                             type: eventType,
                             user: this.currentUser
                           } )
+    },
+    changeVisibility: function () {
+      this.visible = !this.visible
     }
   },
+  components: {
+    currentPost: ( 'current-post', {
+      props: [ "title", "image", "code", "text", "refs" ],
+      data: function () {
+        return { postIsVisible: false }
+      },
+      methods: {
+        changeVisibility: function () {
+          this.postIsVisible = !this.postIsVisible
+        },
+        openRef: function ( ref ) {
+          window.open (ref, "_blank")
+        }
+      },
+      template: `
+        <section>
+          <transition name="slideLeft">
+            <h3 class="menu-item" @click="changeVisibility">{{ title }}</h3>
+          </transition>
+          <transition name="slideUp">
+            <img v-if="image && this.postIsVisible" 
+                  :src="image"/>
+          </transition>
+          <transition name="slideUp">
+            <pre v-if="code && this.postIsVisible">
+              {{ code }}
+            </pre>
+          </transition>
+          <transition name="slideUp">
+            <p v-if="text && this.postIsVisible">
+              {{ text }}
+            </p>
+          </transition>
+          <ol v-for="sample in refs">
+              <li class="menu-item"
+                @click="openRef(sample)"
+                >
+              </li>
+          </ol>
+        </section>
+      `
+    })
+  },
   template: `<section class="article">
-              <transition-group name="articles" tag="div">
-                <div v-for="item in currentUserPosts"
-                      :key="item.head">
-                  <transition name="articles" tag="div">
-                  <h3>{{ item.head }}</h3>
-                  </transition>
-                  <transition name="articles" tag="div">
-                  <img v-if="item.pict" :src="item.pict"/>
-                  </transition>
-                  <transition name="articles" tag="div">
-                  <pre>{{ item.code }}</pre>
-                  </transition>
-                  <p>{{ item.text }}</p>
-                  <a v-if="item.ref"
-                      v-for="sample in item.ref"
-                      class="menu-item"
-                      :href="sample" target="_blank">
-                    <span :v-if="item.ref">Пример</span>
-                  </a>
+                <div v-for="item in currentUserPosts">
+                  <current-post :title="item.head"
+                                :image="item.pict"
+                                :code="item.code"
+                                :text="item.text"
+                                :refs="item.ref">
+                  </current-post>
                   <hr/>
                 </div>
-              </transition-group>
              </section>`
 }
