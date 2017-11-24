ca33e37d836ddc3fea8a01e29b4fbb227ca9f329
garevna <garevna@mail.ru>
2017-11-24 17:06:16 +0200

    Update test_json_saver.js

--- workers/test_json_saver.js
+++ workers/test_json_saver.js
@@ -1,7 +1,7 @@
 function get_transport() {
 	return XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP")
 };
-// ============================================================================================= SAVE FILE TO SERVER
+// =================================================================== SAVE FILE TO SERVER
 this.save_file = function ( phpFileURL, dataFileURL, dataFileContent ) {
 	
 	var data = JSON.stringify ( {
@@ -11,13 +11,15 @@ this.save_file = function ( phpFileURL, dataFileURL, dataFileContent ) {
 	save_request = get_transport()
 	save_request.onreadystatechange = function() {
 		if ( save_request.readyState === 4 ) 
-				console.log ( "save_request.responseText: ", save_request.responseText )
+				console.log ( "save_request.responseText: ", 
+					     save_request.responseText )
 				postMessage( save_request.status === 200 )
 	}
 	
 	save_request.open ( "POST", phpFileURL, true )
 	save_request.setRequestHeader ( "X-Requested-With", "XMLHttpRequest" )
-	save_request.setRequestHeader ( "Content-Type", "application/json; charset=utf-8" )
+	save_request.setRequestHeader ( "Content-Type", 
+				       "application/json; charset=utf-8" )
   save_request.send ( data )
 }
 // ====================================================== READ FILE FROM SERVER
@@ -26,12 +28,12 @@ this.read_file = function ( dataFileURL ) {
 	read_request.onreadystatechange = function() {
 		if ( read_request.readyState < 4 ) return
 		if ( read_request.status === 200 ) {
-				this.data = read_request.responseText
-				postMessage( read_request.responseText )
-			} else {
-					console.info ( "Reading file error: " + read_request.responseText )
-					postMessage( false )
-			}
+			this.data = read_request.responseText
+			postMessage( read_request.responseText )
+		} else {
+			console.info ( "Reading file error: " + 
+				      read_request.responseText )
+			postMessage( false )
 		}
 	}
 	read_request.open ( "POST", dataFileURL )
