427a5fb8aab4f2823c31d080a6ed9cfdbdea439d
garevna <garevna@mail.ru>
2018-01-05 13:13:23 +0200

    Add files via upload

---
+++ components/HomePage.js
@@ -0,0 +1,8 @@
+const HomePage = ( 'home-page', {
+  template: `
+    <div class="home-page">
+      <img src="./images/my-photo.png" class="my-photo"/>
+      <h3>Front End</h3>
+    </div>
+  `
+})
